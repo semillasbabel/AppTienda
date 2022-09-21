7c26c0b96772c17f84333fa0e69171c08eb787d5
Hugoch13 <hugojachb13@gmail.com>
2022-09-21 09:22:09 -0600

    Prueba Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tienda de productos</Text>
+      <Text>Tienda de productos para moviles</Text>
       <StatusBar style="auto" />
     </View>
   );
