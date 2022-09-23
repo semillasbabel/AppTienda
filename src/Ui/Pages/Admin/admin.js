e90d9689d95892551599f68f23b68203044b79c1
Hugoch13 <hugojachb13@gmail.com>
2022-09-23 09:09:29 -0600

    cAMBIOS MINIMOS

---
+++ src/Ui/Pages/Admin/admin.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+import { useNavigation } from "@react-navigation/native";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { singOutController } from "../../../Domain/Repositories/Firebase/Auth/singOut"
+
+function Admin() {
+  const navigation = useNavigation();
+  const auth = getAuth();
+
+  async function singout(){
+      if (await singOutController(auth)) {
+        console.log("Se cerro la sesión")
+        navigation.navigate("Loggin")
+      } else {
+        console.log("Error al cerrar la sesión")
+      }
+  }
+
+ 
+
+  return (
+    <View>
+      <View style={{height:100}}/>
+      <Text>administrador</Text>
+      <Button
+        title='Cerrar Sesion'
+        onPress={singout}
+      />
+    </View>
+  )
+}
+
+export default Admin
