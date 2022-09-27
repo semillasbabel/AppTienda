5c0c134969773e83d0ca6d54ba711e2848f63392
keii06 <k.eiguar1997@gmail.com>
2022-09-27 10:37:19 -0600

    Se corrigio el problema del mensaje en amarillo

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { View, Image, TouchableOpacity, ImageBackground} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from ']);
 
 import Loggin from "./src/Ui/Pages/Loggin/loggin";
 import Register from "./src/Ui/Pages/Register/register";
