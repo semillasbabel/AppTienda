d6d69a910fd4f019abfb76e4a5733b01223c9c9f
keii06 <k.eiguar1997@gmail.com>
2022-09-29 12:44:23 -0600

    Se cambio el nombre de la carpeta User a Client

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and w
 
 import Loggin from "./src/Ui/Pages/Loggin/loggin";
 import Register from "./src/Ui/Pages/Register/register";
-import Client from "./src/Ui/Pages/User/Client";
+import Client from "./src/Ui/Pages/Client/Client";
 import Admin from "./src/Ui/Pages/Admin/admin";
 
 const Stack = createNativeStackNavigator();
