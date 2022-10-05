5adf7a632d0d0df45c2968ed6293875099cb2b5c
Hugoch13 <hugojachb13@gmail.com>
2022-10-05 08:07:28 -0600

    cambios de estilos en pantalla products y otras

---
+++ src/Ui/Pages/Client/Views/Shop/Components/styles-details/style-details.js
@@ -0,0 +1,29 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+
+ViewPrincipal:{
+    flex: 1,
+    backgroundColor: "transparent", 
+    marginBottom: 20
+},
+ViewSecundario:{
+    alignSelf:"center",
+     height: "40%", 
+     width:"70%", 
+     marginBottom: 20
+},
+cardImage:{
+    margin:10, 
+    height:"100%", 
+    width:"90%", 
+    alignSelf:"center"
+},
+textView:{
+    fontSize:18
+},
+
+
+
+
+})
