05fbfb894c70b3cc1300bdb76a75a77ecb614ea2
keii06 <k.eiguar1997@gmail.com>
2022-10-06 23:30:09 -0600

    Se agrego jest al proyecto

--- requirements.bat
+++ requirements.bat
@@ -1,5 +1,7 @@
+npm install
 npm i react-native-keyboard-aware-scroll-view --save
 npm i jest-expo --save-dev
 npm install @react-native-async-storage/async-storage
 npm install react-native-super-grid
 npm install -g firebase-tools
+npx expo install jest-expo jest
